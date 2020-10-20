53cea473768121f36d0a84f806ec63635c826f6f
Komplexnupe <leonard.love87@icloud.com>
2020-10-20 10:10:36 -0400

    inital commit

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,9 +7,9 @@ const Nav = () => {
 return (
   
   <ul className="nav">
-    <li><a href="#About" active><i class="fas fa-user-circle" style={{fontSize: 48}}></i><br/> About Me</a></li>
-    <li style={{marginLeft: 50}}><a href="https://drive.google.com/file/d/1BPanj7Ujy8ywv0nd8tDA8yvqtW-l9bqM/view?usp=sharing" hover target="_blank" rel="noopener noreferrer"><i class="fas fa-file-pdf" style={{fontSize: 48}}></i><br/> Resume</a></li>
-    <li style={{marginLeft: 50}}><a href="#projects" hover><i class="fas fa-laptop-code" style={{fontSize: 48}}></i><br/> My Work</a></li>
+    <li><a href="#About" active><i class="fas fa-user-circle" style={{fontSize: 56}}></i><br/> About Me</a></li>
+    <li style={{marginLeft: 50}}><a href="https://drive.google.com/file/d/1BPanj7Ujy8ywv0nd8tDA8yvqtW-l9bqM/view?usp=sharing" hover target="_blank" rel="noopener noreferrer"><i class="fas fa-file-pdf" style={{fontSize: 56}}></i><br/> Resume</a></li>
+    <li style={{marginLeft: 50}}><a href="#projects" hover><i class="fas fa-laptop-code" style={{fontSize: 56}}></i><br/> My Work</a></li>
   </ul>
 
 )
