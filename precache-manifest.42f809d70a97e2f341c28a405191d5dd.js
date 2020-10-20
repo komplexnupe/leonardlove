5349850ad97abe02cb8ed467216581467aeffb53
Komplexnupe <leonard.love87@icloud.com>
2020-10-20 10:26:04 -0400

    Updates

---
+++ precache-manifest.42f809d70a97e2f341c28a405191d5dd.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "234bb2e565dfda9a03f905b217fd32fe",
+    "url": "/leonardlove/index.html"
+  },
+  {
+    "revision": "5fd232d158555af6a7f0",
+    "url": "/leonardlove/static/css/2.fa8fa158.chunk.css"
+  },
+  {
+    "revision": "baca3e1c1dff71e4a189",
+    "url": "/leonardlove/static/css/main.52818730.chunk.css"
+  },
+  {
+    "revision": "5fd232d158555af6a7f0",
+    "url": "/leonardlove/static/js/2.c827dec9.chunk.js"
+  },
+  {
+    "revision": "1b9cc396521eecb2045c787139670fba",
+    "url": "/leonardlove/static/js/2.c827dec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baca3e1c1dff71e4a189",
+    "url": "/leonardlove/static/js/main.a686ae99.chunk.js"
+  },
+  {
+    "revision": "068545e0215abb7253ea",
+    "url": "/leonardlove/static/js/runtime-main.fc0e3cae.js"
+  },
+  {
+    "revision": "fc263a4b65429fc1e57967359901ed90",
+    "url": "/leonardlove/static/media/Black-website-background-black.fc263a4b.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/leonardlove/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/leonardlove/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "1247b927e5bc12a02802b08af97d0ba9",
+    "url": "/leonardlove/static/media/bloc.1247b927.jpg"
+  },
+  {
+    "revision": "476d66f5c3939f95e25845c6734008b0",
+    "url": "/leonardlove/static/media/cocktellme.476d66f5.png"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/leonardlove/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/leonardlove/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/leonardlove/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/leonardlove/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/leonardlove/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/leonardlove/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/leonardlove/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/leonardlove/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/leonardlove/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/leonardlove/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/leonardlove/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/leonardlove/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/leonardlove/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/leonardlove/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/leonardlove/static/media/fa-solid-900.8e1ed89b.woff2"
+  },
+  {
+    "revision": "51c9b634c101bb781cb2c68fa040077e",
+    "url": "/leonardlove/static/media/fitlink.51c9b634.png"
+  },
+  {
+    "revision": "06fe34402ebef640a9cab83888ae2160",
+    "url": "/leonardlove/static/media/saint-george.06fe3440.png"
+  }
+]);
